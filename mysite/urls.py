ce94a28c4aa65c52f1001676bb51b0ae55c10811
vinakum6 <53523817+vinakum6@users.noreply.github.com>
2020-04-13 13:38:35 +0100

    Added the polls/

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 #Test
 urlpatterns = [
-    path('', include('polls.urls')),
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
