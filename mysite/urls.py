96fe336136dbcd0f3bbf1f586496cf0a238ef3d0
vinakum6 <53523817+vinakum6@users.noreply.github.com>
2020-04-13 14:03:56 +0100

    Removed the polls/

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 #Test
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
